8cb827b93e1904a25241e2e0e2c1a68a5b6b8023
Nadin-Machulska <rumbachka60@gmail.com>
2022-09-07 15:46:50 +0300

    fixed search movies, add style

--- second.js
+++ second.js
@@ -4,7 +4,9 @@ const submitButton = document.querySelector('.form__button');
 const outputTitle = document.querySelector('.output__title');
 const outputImg = document.querySelector('.output__img img');
 const servResponce = document.querySelector('.title');
-const wrapper = document.querySelector('.wrapper')
+const wrapper = document.querySelector('.wrapper');
+const resultList = document.querySelector('.result-list');
+const option = document.querySelectorAll('.type__select option');
 
 // const url = `http://www.omdbapi.com/?apikey=2b36207a&s=${s}&y=${y}`
 
@@ -14,12 +16,25 @@ submitButton.addEventListener('click', sumbitRequest)
 
 function sumbitRequest(event){
     event.preventDefault();
-    
+
+    resultList.innerText = '';
     
     let s = `${movieNameInput.value}`;
     let y = `${movieYearRelease.value}`;
+    let type = ``;
 
-    const url = `http://www.omdbapi.com/?apikey=2b36207a&s=${s}&y=${y}`
+    option.forEach(getOptionValue);
+
+    function getOptionValue(element){
+
+            if (element.selected){
+                return type = `${element.value}`
+            };
+        }
+
+
+
+    const url = `http://www.omdbapi.com/?apikey=2b36207a&s=${s}&y=${y}&type=${type}`
     
     const xhr = new XMLHttpRequest();
 
@@ -30,27 +45,34 @@ function sumbitRequest(event){
         xhr.onload = () => {
            
             let a = JSON.parse(xhr.responseText);
-        
-        
+            // let today = new Date();
+            // let year = today.getFullYear();
             console.log(a.Search);
             a.Search.forEach(e => createResultOfSearch (e.Title, e.Poster));
         };
         
         function createResultOfSearch (title, source){
+
+            let resultItem = document.createElement('li');
+            resultItem.classList.add('result-item');
+            resultList.append(resultItem)
+
             let divTitle = document.createElement("div");
             divTitle.setAttribute("class", "output__title");
             divTitle.innerHTML += `${title}`;
         
-            wrapper.append(divTitle);
+            resultItem.append(divTitle);
+
             let divImg = document.createElement("img");
         
             if (source == "N/A"){
-                divImg.setAttribute('src', './image/http-error-404-not-found.png');
+                divImg.setAttribute('src', 'images/erorr-404.jpg');
             } else {
                 divImg.setAttribute('src', `${source}`);
             }
         
-            wrapper.append(divImg)
+            resultItem.append(divImg)
         }
 }
 
+document.querySelectorAll('.type__select')
